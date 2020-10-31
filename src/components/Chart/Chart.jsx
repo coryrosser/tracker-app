c609b26247fa13fabd6c7911357abfe98e7791ac
Cory <coryrosser@gmail.com>
2020-10-31 12:55:22 -0400

    components and folders made

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Chart = () => {
+  return (
+    <p> Chart </p>
+  )
+}
+export default Chart
---
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const CountryPicker = () => {
+  return (
+    <p> CountryPicker </p>
+  )
+}
+export default CountryPicker
