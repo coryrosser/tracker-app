d4e1cb76c58eae9eb5596ed4e8c7626ce039aef4
Cory <coryrosser@gmail.com>
2020-11-03 00:56:23 -0500

    chart

--- src/App.js
+++ src/App.js
@@ -27,9 +27,11 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <img className={styles.image} url='https://i.ibb.co/7QpKsCX/image.png' alt="COVID-19" />
+        <img className={styles.image} src='https://i.ibb.co/7QpKsCX/image.png' alt="COVID-19" />
         <Cards data={data} />
         <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+        <Chart data={data} country={country} /> 
         <Chart data={data} country={country} /> 
       </div>
     );
